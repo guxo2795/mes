c81f4458814309e3b1ab6d6f230ffacf124ae4fc
guxo2795 <guxo1211@gmail.com>
2025-04-07 11:22:24 +0900

    git init

---
+++ src/main/java/com/wimir/bae/BaeApplication.java
@@ -0,0 +1,13 @@
+package com.wimir.bae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wimir/bae/BaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimir.bae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
